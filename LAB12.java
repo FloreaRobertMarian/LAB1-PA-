2a4879f679e554c3d214f8c363195515b10e7ae5
FloreaRobertMarian <79140571+FloreaRobertMarian@users.noreply.github.com>
2021-02-16 14:18:16 +0200

    Add files via upload

---
+++ LAB12.java
@@ -0,0 +1,28 @@
+
+package lab1.pkg2;
+
+/**
+ *
+ * @author ciric
+ */
+public class LAB12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Hello World!");
+       String[] array =new String[] {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+      int n = (int) (Math.random() * 1_000_000);
+      n=n*3;
+      int a=  Integer.toBinaryString(10101);
+      int b= Integer.parseInt(FF);
+      y=add(a,b,n);
+      n=n*6;
+      System.out.println("Willy-nilly,this semestre I will learn");
+      for(int i=0; i<array.lenght ;i++)
+          Sistem.out.print(array[i]+" ");
+      int[][] m= new int[][];
+    }
+    
+}
