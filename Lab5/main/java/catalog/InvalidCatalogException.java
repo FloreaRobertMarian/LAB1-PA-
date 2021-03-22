ddc0122b0f52585fb7c167038ce5e63c8945865c
FloreaRobertMarian <79140571+FloreaRobertMarian@users.noreply.github.com>
2021-03-22 19:59:07 +0200

    Delete Lab5/main/java directory

--- Lab5/main/java/catalog/InvalidCatalogException.java
+++
@@ -1,8 +0,0 @@
-package catalog;
-
-public class InvalidCatalogException extends Exception {
-    public InvalidCatalogException(Exception ex) {
-        super("Invalid catalog file.", ex);
-    }
-}
-
--- Lab5/main/java/shell/commands/InvalidCommandException.java
+++
@@ -1,9 +0,0 @@
-package shell.commands;
-
-import catalog.InvalidCatalogException;
-
-public class InvalidCommandException extends Exception {
-    public InvalidCommandException(String message) {
-        super(message);
-    }
-}
